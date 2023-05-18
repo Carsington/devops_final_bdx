2a2c17d67854ba58f8280b05c8537a83baea2b84
djawed <krairij@gmail.com>
2023-05-18 10:37:45 +0200

    add files

--- Rocky/Oracle/install_oracle.sh
+++ Rocky/Oracle/install_oracle.sh
@@ -1,114 +1,118 @@
-hostnamectl set-hostname oracle-02.centlinux.com
-echo "192.168.186.165 oracle-02.centlinux.com oracle-02" >> /etc/hosts
+# Executer ses commande dans Rocky
+su -
 nmcli dev
 sudo nmcli connection add con-name hostonly ifname ens192 type ethernet autoconnect yes
 sudo nmcli connection add con-name nat ifname ens160 type ethernet autoconnect yes
 nmcli dev
+ip a
+# Aller dans moba
 su -
-dnf makecache
-dnf update -y
-reboot
-cat /etc/rocky-release
-uname -r
-cat /sys/kernel/mm/transparent_hugepage/enabled
-su -
-vi /etc/default/grub # Ajouter transparent_hugepage=never
-grub2-mkconfig -o /boot/grub2/grub.cfg
-reboot
-cat /sys/kernel/mm/transparent_hugepage/enabled
-su -
-
-dnf install -y  \
-bc \
-binutils \
-compat-openssl10 \
-elfutils-libelf \
-glibc \
-glibc-devel \
-ksh \
-libaio \
-libXrender \
-libX11 \
-libXau \
-libXi \
-libXtst \
-libgcc \
-libnsl \
-libstdc++ \
-libxcb \
-libibverbs \
-make \
-policycoreutils \
-policycoreutils-python-utils \
-smartmontools \
-sysstat \
-libnsl2 \
-net-tools \
-nfs-utils \
-unzip
-
-
-vi /etc/security/limits.d/30-oracle.conf
-/*
-oracle   soft   nofile   1024
-oracle   hard   nofile   65536
-oracle   soft   nproc    16384
-oracle   hard   nproc    16384
-oracle   soft   stack    10240
-oracle   hard   stack    32768
-oracle   hard   memlock  134217728
-oracle   soft   memlock  134217728
-oracle   soft   data     unlimited
-oracle   hard   data     unlimited
-*/
-
-vi /etc/sysctl.d/98-oracle.conf
-*/
-fs.file-max = 6815744
-kernel.sem = 250 32000 100 128
-kernel.shmmni = 4096
-kernel.shmall = 1073741824
-kernel.shmmax = 4398046511104
-kernel.panic_on_oops = 1
-net.core.rmem_default = 262144
-net.core.rmem_max = 4194304
-net.core.wmem_default = 262144
-net.core.wmem_max = 1048576
-net.ipv4.conf.all.rp_filter = 2
-net.ipv4.conf.default.rp_filter = 2
-fs.aio-max-nr = 1048576
-net.ipv4.ip_local_port_range = 9000 65500
-/*
-sysctl -p
-sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
-setenforce permissive
-firewall-cmd --permanent --add-port=1521/tcp
-firewall-cmd --reload
-mkdir -p /u01/app/oracle/product/19.3.0/dbhome_1
-mkdir -p /u02/oradata
-chown -R oracle:oinstall /u01 /u02
-chmod -R 775 /u01 /u02
+hostnamectl set-hostname oracledb19col8.rishoradev.com
+echo "192.168.186.170 oracledb19col8.rishoradev.com oracledb19col8" >> /etc/hosts
+ifconfig
+hostname
+cp /tmp/oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm .
+dnf install -y oracle-database-preinstall-19c-1.0-2.el8.x86_64.rpm
+yum update -y --skip-broken 
+vi /etc/selinux/config # SELINUX=permissive
+systemctl stop firewalld
+systemctl disable firewalld
+passwd oracle
 su - oracle
-vi ~/.bash_profile
-*/
+mkdir -p /home/oracle/u01/app/oracle/product/19c/dbhome_1
+mkdir -p /home/oracle/u02/oradata
+chown -R oracle:oinstall /home/oracle/u01 /home/oracle/u02
+chmod -R 777 /home/oracle/u01 /home/oracle/u02
+mkdir scripts
+cat > /home/oracle/scripts/setEnv.sh <<EOF
 # Oracle Settings
 export TMP=/tmp
-export TMPDIR=$TMP
+export TMPDIR=\$TMP
 
-export ORACLE_HOSTNAME=oracle-02.centlinux.com
+export ORACLE_HOSTNAME=oracledb19col8.rishoradev.com
 export ORACLE_UNQNAME=cdb1
 export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=$ORACLE_BASE/product/19.3.0/dbhome_1
+export ORACLE_HOME=\$ORACLE_BASE/product/19c/dbhome_1
 export ORA_INVENTORY=/u01/app/oraInventory
 export ORACLE_SID=cdb1
 export PDB_NAME=pdb1
 export DATA_DIR=/u02/oradata
 
-export PATH=$ORACLE_HOME/bin:$PATH
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=\$ORACLE_HOME/bin:\$PATH
 
-export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
-export CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
-*/
-source ~/.bash_profile
+export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+EOF
+echo ". /home/oracle/scripts/setEnv.sh" >> /home/oracle/.bash_profile
+cd u01/app/oracle/product/19c/dbhome_1/
 cp /tmp/LINUX.X64_193000_db_home.zip .
-unzip LINUX.X64_193000_db_home.zip -d $ORACLE_HOME
+ls -lrt
+unzip -q LINUX.X64_193000_db_home.zip
+ls
+./runInstaller
+export CV_ASSUME_DISTID=OEL7.6
+./runInstaller
+cd /home/oracle/u01/app/oraInventory/
+./orainstRoot.sh
+cd oracle/product/19c/dbhome_1/
+./root.sh
+cd
+lsnrctl start
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname ${ORACLE_SID} -sid  ${ORACLE_SID} -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                                                    \
+     -sysPassword Welcome1                                                 \
+     -systemPassword Welcome1                                              \
+     -createAsContainerDatabase true                                           \
+     -numberOfPDBs 1                                                           \
+     -pdbName ${PDB_NAME}                                                      \
+     -pdbAdminPassword Welcome1                                            \
+     -databaseType MULTIPURPOSE                                                \
+     -memoryMgmtType auto_sga                                                  \
+     -totalMemory 2000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                     \
+     -ignorePreReqs
+
+sqlplus / as sysdba
+Select BANNER_FULL from v$version;
+[13:16]
+cat > /home/oracle/scripts/start_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+dbstart $ORACLE_HOME
+EOF
+[13:17]
+cat > /home/oracle/scripts/stop_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+dbshut $ORACLE_HOME
+EOF
+[13:17]
+chown -R oracle:oinstall /home/oracle/scripts
+chmod u+x /home/oracle/scripts/*.sh
+sqlplus / as sysdba <<EOF
+alter system set db_create_file_dest='${DATA_DIR}';
+alter pluggable database ${PDB_NAME} save state;
+exit;
+EOF
+su -
+dnf install git
+su - oracle
+git clone -b ora-nodocker https://github.com/matthcol/dbmovie
+cd dbmovie/sql
+sqlplus / as sysdba
+alter session set "_ORACLE_SCRIPT"=true;
+@00-user.sql
+sqlplus movie/password @ movie_all.sql
+
