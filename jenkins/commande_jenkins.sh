db0ffd5a7e383d9fde821fc43c8df4f8c8b3abce
djawed <krairij@gmail.com>
2023-05-19 20:29:05 +0200

    modification jenkins

--- jenkins/commande_jenkins.sh
+++ jenkins/commande_jenkins.sh
@@ -1,24 +1,57 @@
+#Changer le host et hostname
+sudo sed -i "s/debian/jenkins/g" /etc/hostname /etc/hosts
+# Redemarrer ma vm
+sudo reboot
+# Créer un utilisateur jenkins avec un home
+sudo useradd -m -s /bin/bash jenkins
+# Mettre jenkins dans le groupe sudo
+sudo usermod -aG sudo djawed
+# Redemarrer ma vm
+sudo reboot
+# Se connecter à l'utilisateur jenkins
+su -
+su - jenkins
+# crèer les clés
+ssh-keygen
+# Dèfinir les droits
+chmod 700 .ssh
+# Se placer dans le dossier .ssh
+cd .ssh
+# Supprimer les 2 clès
+rm id*
+# Se connecter en root
+su
+# Dèplacer les 2 clès present dans /tmp
+mv /tmp/id_rs* .
+# Dèfinir les droits
+chmod 600 id_rsa
+# Rendre jenkins propriétaire de ce dossier
+chown jenkins:jenkins /home/jenkins/.ssh/id*
+# Se connecter à l'utilisateur jenkins
+su jenkins
+# créer un fichier config
+vi config # Coller les éléments qui suient
+Host github.com
+        Hostname github.com
+        IdentityFile=/home/jenkins/.ssh/id_rsa
+# Dèfinir les droits
+chmod 600 config
+# Se positionner dans le home de l'utilisateur jenkins
+cd
+# Cloner le git proteger de Matthias
+git clone git@github.com:matthcol/movieapi2k3.git
+git clone -b ora-nodocker https://github.com/matthcol/dbmovie
+git clone -b devf1 https://github.com/matthcol/movie_angular.git
+# Mettre à jours mes logiciel
+sudo apt update
+sudo apt upgrade
+# Installer les différents logiciels
+sudo apt install openjdk-17-jdk
+sudo apt install maven
+# Telecharger le fichier jenkins.war dans le home
+wget https://get.jenkins.io/war-stable/2.387.2/jenkins.war
+# Lancer jenkins
+java -jar jenkins.war
 
-   19  sudo sed -i "s/debian/jenkins/g" /etc/hostname /etc/hosts
-   20  sudo reboot
-   21  sudo useradd -m -s /bin/bash jenkins
-   22  sudo su jenkins
-   23  sudo apt update
-   24  sudo apt install openjdk-17-jdk
-   25  sudo apt install maven
-   26  sudo su jenkins
-   27  sudo su jenkins
-   28  cd /tmp
-   29  ls
-   30  scp movieapi.jar 192.168.186.173:/tmp
-   31  scp application.properties 192.168.186.173:/tmp
-   32  cd
-   33  ls
-   34  tree
-   35  cd
-   36  ls
-   37  su
-   38  cd /tmp/
-   39  scp movieapi.jar djawed@192.168.186.175:/tmp
-   40  scp movie_angular.tar.gz  djawed@192.168.186.175:/tmp
-   41  history
+
+
